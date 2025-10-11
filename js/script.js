8d5ab4b071118e5ca2a76393c555582e26f58a99
Ajadenis <denisfirmansyahramadhan@gmail.com>
2025-10-12 01:31:53 +0700

    update analytics

--- js/script.js
+++ js/script.js
@@ -12,10 +12,3 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
-
-// Alnalytics Vercel
-window.va =
-  window.va ||
-  function () {
-    (window.vaq = window.vaq || []).push(arguments);
-  };
